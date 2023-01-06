2b63e6e7adba985aa6979f9cf88befc09382b256
conwads <Wadsworthjc97@gmail.com>
2023-01-06 13:33:58 -0700

    Very first commit for portal page

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+let date = new Date();
+let year = date.getFullYear();
+
+document.queaySelector('h1').innerHTML = '&copy;' + year;
+
+let currentdate = document.lastModified 
+
+document.querySelector('div').textContent = 'Last Updated: ' + currentdate;
+
+
+
