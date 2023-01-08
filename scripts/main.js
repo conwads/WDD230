3098a5cfa7c722160c1f23a30132721cbcf527c6
conwads <Wadsworthjc97@gmail.com>
2023-01-07 20:39:18 -0700

    Final changes to portal page Jan 7th

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,15 @@
-alert("hello");
+
 let date = new Date();
 let year = date.getFullYear();
 
-document.querySelector('h1').innerHTML = '&copy;' + year;
+document.querySelector('h4').innerHTML = '&copy;' + year;
 
-let currentdate = document.lastModified 
+let currentdate = document.lastModified
 
 document.querySelector('div').textContent = 'Last Updated: ' + currentdate;
 
-
+function lastUpdated() {
+    let a = document.lastModified;
+    document.getElementById("lastModified").innerHTML = a;
+}
 
