9912d7097120a32cb7b7544b38a29e6a714f0d0e
conwads <wadsworthjc97@gmail.com>
2023-03-17 17:18:43 -0600

    changed some stuff

--- lesson 4/script/lesson4.js
+++ lesson 4/script/lesson4.js
@@ -38,30 +38,3 @@ if (day == 1 || day == 2){
 
 localStorage.setItem('name', 'conner wadsworth - Permanent');
 sessionStorage.setItem('class', 'WDD230- not Permanent');
-
-// if(!localStorage.getItem('lastvisit')) {
-//     localStorage.getItem('lastvisit', Date.now())
-
-// } else {
-//     let currentdate = Date.now(),
-//     let lastdate = 
-// }
-
-
-// const apiURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Augusta%20Georgia?unitGroup=us&key=Z8ZX6JBPMWA5KK83T2AHVFWE6&contentType=json";
-// const getWeather = async () => {
-//     const response = await fetch(apiURL);
-//     const jsObject = await response.json();
-//     console.log(jsObject);
-  
-//     document.querySelector('#tempNumbers').textContent = ((jsObject.main.temp - 273.15) * 1.8 + 32).toFixed(2);
-//     document.querySelector('#ws').textContent = jsObject.wind.speed;
-//     document.querySelector('#weatherCondition').textContent = jsObject.weather[0].description;
-
-//     const iconsrc= `https://openweathermap.org/img/wn/${jsObject.weather[0].icon}@2x.png`;
-  
-//     document.querySelector('#weatherPic').textContent = iconsrc;
-//     document.querySelector('#weatherPic').setAttribute('src', iconsrc)
-//     document.querySelector('#weatherPic').setAttribute('alt', sunny);
-//   };
-// getWeather();
