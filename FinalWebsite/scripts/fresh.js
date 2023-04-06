01d04053c89aca7e253f5dd54c452a7d0d64b5bc
conwads <wadsworthjc97@gmail.com>
2023-04-05 21:41:07 -0600

    Before Final Changes

--- FinalWebsite/scripts/fresh.js
+++ FinalWebsite/scripts/fresh.js
@@ -1,91 +1,126 @@
+const url = "https://andejuli.github.io/wdd230/fruit.json";
 
-async function processData() {
-    const fruits = await getFruit();
+async function getFruitData() {
+    const response = await fetch(url);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
 
-    buildOptions(fruits);
-
-    for (let i = 1; i < 4; i++) {
-        document.querySelector(`#fruit-${i}`).addEventListener('change', function () {
-            calculateNutrition(fruits);
-        });
-    }
-}
-
-function buildOptions(fruits) {
-
-    for (let i = 1; i < 4; i++) {
-        let fruitSelection = document.querySelector(`#fruit-${i}`);
-
-        fruits.forEach(fruit => {
-            let option = document.createElement('option');
-            option.value = fruit.name;
-            option.textContent = fruit.name;
-            
-            fruitSelection.appendChild(option);
-        });
-    }
-}
-
-function getSelectedFruit() {
-    let fruits = [];
-
-    for (let i = 1; i < 4; i++) {
-        fruits.push(document.querySelector(`#fruit-${i}`).value)
+    for (i = 0; i < data.length; i++) {
+        var sel = document.createElement("option");
+        sel.innerHTML = data[i].name;
+        sel.value = data[i].name;
+        document.getElementById("one_fruits").appendChild(sel);
     }
 
-    return fruits
-}
-
-function calculateNutrition(fruits) {
-    const selectedFruits = getSelectedFruit();
-
-    const nutrients = getNutritionFacts(selectedFruits, fruits);
-
-    document.querySelector('#calories').textContent = nutrients.calories;
-    document.querySelector('#fat').textContent = nutrients.fat;
-    document.querySelector('#sugar').textContent = nutrients.sugar;
-    document.querySelector('#carbohydrates').textContent = nutrients.carbohydrates;
-    document.querySelector('#protein').textContent = nutrients.protein;
-}
-
-function orderSubmit(event) {
-    let currentDate = new Date();
-    console.log(currentDate);
-
-    const values = {
-        fname: document.querySelector('#fname').value,
-        email: document.querySelector('#email').value,
-        phone: document.querySelector('#phone').value,
-        fruit1: document.querySelector('#fruit-1').value,
-        fruit2: document.querySelector('#fruit-2').value,
-        fruit3: document.querySelector('#fruit-3').value,
-        special: document.querySelector('#special').value,
-        orderDate: currentDate
+    for (i = 0; i < data.length; i++) {
+        var sel = document.createElement("option");
+        sel.innerHTML = data[i].name;
+        sel.value = data[i].name;
+        document.getElementById("two_fruits").appendChild(sel);
     }
 
-    if (! localStorage.getItem('orders')) {
-        let orders = [values];
-        localStorage.setItem('orders', JSON.stringify(orders));
-    } else {
-        let orders = JSON.parse(localStorage.getItem('orders'));
-
-        orders.push(values);
-
-        localStorage.setItem('orders', JSON.stringify(orders));
+    for (i = 0; i < data.length; i++) {
+        var sel = document.createElement("option");
+        sel.innerHTML = data[i].name;
+        sel.value = data[i].name;
+        document.getElementById("three_fruits").appendChild(sel);
     }
-    
-    event.preventDefault();
-    form.reset();
 
-    displayDrinks();
+
+    // Order message
+    const freshElement = document.getElementById("fresh_content");
+    const formElement = document.forms[0];
+    formElement.addEventListener("submit", function (e) {
+        e.preventDefault();
+        freshElement.innerHTML =
+            "Hello " +
+            formElement.firstname.value +
+            "! You ordered: a " +
+            formElement.message.value + " " +
+            formElement.one_fruits.value +
+            ", " +
+            formElement.two_fruits.value +
+            ", " +
+            formElement.three_fruits.value +
+            " Smoothie ";
+        freshElement.style.display = "block";
+
+        let nutritions1;
+        let nutritions2;
+        let nutritions3;
+        let calories1;
+        let calories2;
+        let calories3;
+        let carbohydrates1;
+        let carbohydrates2;
+        let carbohydrates3;
+        let fat1;
+        let fat2;
+        let fat3;
+        let protein1;
+        let protein2;
+        let protein3;
+        let sugar1;
+        let sugar2;
+        let sugar3;
+        let date = new Date();
+
+
+        data.forEach(showFruit);
+        function showFruit(item) {
+            console.log(item.name);
+
+
+            if (item.name == formElement.one_fruits.value) {
+                nutritions1 = item.nutritions;
+                calories1 = item.nutritions.calories;
+                carbohydrates1 = item.nutritions.carbohydrates;
+                fat1 = item.nutritions.fat;
+                protein1 = item.nutritions.protein;
+                sugar1 = item.nutritions.sugar;
+            }
+
+            if (item.name == formElement.two_fruits.value) {
+                nutritions2 = item.nutritions;
+                calories2 = item.nutritions.calories;
+                carbohydrates2 = item.nutritions.carbohydrates;
+                fat2 = item.nutritions.fat;
+                protein2 = item.nutritions.protein;
+                sugar2 = item.nutritions.sugar;
+            }
+
+            if (item.name == formElement.three_fruits.value) {
+                nutritions3 = item.nutritions;
+                calories3 = item.nutritions.calories;
+                carbohydrates3 = item.nutritions.carbohydrates;
+                fat3 = item.nutritions.fat;
+                protein3 = item.nutritions.protein;
+                sugar3 = item.nutritions.sugar;
+            }
+
+
+
+        }
+        let cal_calc = Number(calories1) + Number(calories2) + Number(calories3);
+        console.log(cal_calc);
+        let carb_calc = Number(carbohydrates1) + Number(carbohydrates2) + Number(carbohydrates3);
+        let fat_calc = Number(fat1) + Number(fat2) + Number(fat3);
+        let protein_calc = Number(protein1) + Number(protein2) + Number(protein3);
+        let sugar_calc = Number(sugar1) + Number(sugar2) + Number(sugar3);
+
+        freshElement.append("Calorie total: " + cal_calc.toFixed(2) + " ") + "<br>";
+        freshElement.append("Carb total: " + carb_calc.toFixed(2) + " \n") + "<br>";
+        freshElement.append("total fat: " + fat_calc.toFixed(2) + " \n") + "<br>";
+        freshElement.append("total protein: " + protein_calc.toFixed(2) + " \n") + "<br>";
+        freshElement.append("total sugar: " + sugar_calc.toFixed(2) + " \n") + "<br>";
+        freshElement.append(date);
+
+
+    });
+
+
+
 }
-  
-const form = document.getElementById("form");
-form.addEventListener("submit", orderSubmit);
-  
-processData();
 
-
-
-let modified = document.querySelector('#modified');
-modified.textContent = document.lastModified;
+getFruitData();
